d67a4553a1f039807de6bbfed113b7ac9258cf85
haz <122119697+creacher4@users.noreply.github.com>
2025-08-13 22:14:47 +0100

    first commit: setup github actions

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # vitepress-arc-theming
-
-[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/creacher4/vitepress-arc-theming)
--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,31 +3,19 @@ import { defineConfig } from 'vitepress';
 // refer https://vitepress.dev/reference/site-config for details
 export default defineConfig({
   lang: 'en-US',
-  title: 'VitePress',
-  description: 'Vite & Vue powered static site generator.',
+  title: 'ARC',
+  description: 'Vite & Vue powered static site template for testing theme and components.',
+  base: '/vitepress-arc-theming',
 
   themeConfig: {
     nav: [
       { text: 'Example', link: '/example' },
-
-      // {
-      //   text: 'Dropdown Menu',
-      //   items: [
-      //     { text: 'Item A', link: '/item-1' },
-      //     { text: 'Item B', link: '/item-2' },
-      //     { text: 'Item C', link: '/item-3' },
-      //   ],
-      // },
-
-      // ...
     ],
 
     sidebar: [
       {
-        // text: 'Guide',
         items: [
           { text: 'Example', link: '/example' },
-          // ...
         ],
       },
     ],
